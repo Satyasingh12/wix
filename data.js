29eb3d6d91576cc44317a9ff673660b813886d0e
Satyasingh12 <106381107+Satyasingh12@users.noreply.github.com>
2022-10-01 19:04:31 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+    mongoLink:'mongodb+srv://Botmaker:satya1234@cluster0.xmak0ny.mongodb.net/?retryWrites=true&w=majority', // mongo-link from cloud.mongodb.com
+    
+    bot_token:'5439303999:AAHoxm59b9h7-6Vb_KN9X0GSIUK0UzJLlNw',//bot token
+    
+    bot_name:'Celotestbot',//bot username without @
+    
+    bot_admin:1752227949,// admin telegram id
+    
+    channelsList : ['@Foxdrop1'],// channels list
+
+    twitter:'https://twitter.com/intent/follow?screen_name=HyperGamingR',//your twitter link
+    
+    reffer_bonus:100,// refferal bonus amount
+    
+    min_wd:3,//min withdrawal amount
+    
+    daily_bonus:11111,// daily bonus amount
+    
+    currency:'Wix', //bot currency
+    
+    payment_channel:'@FOxdrop1'// payment channel username
+    
+    }
